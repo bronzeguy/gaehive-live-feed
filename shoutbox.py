a8cc89cb74dfdd5052c109ada63f042480e14de3
bronzeguy <151421032+bronzeguy@users.noreply.github.com>
2023-12-16 18:00:02 -0600

    Add files via upload

---
+++ shoutbox.py
@@ -0,0 +1,28 @@
+import requests
+import time
+
+old = 0
+while True:
+    time.sleep(1)
+    x = requests.get("https://api.scratch.mit.edu/studios/5842709/comments")
+    if x.status_code != 200:
+        raise SystemExit("Did not get a 200, halting")
+    x = x.json()
+    if old == x:
+        continue
+    res = []
+    for i in x:
+        found = 0
+        if old == 0:
+            break
+        for j in old:
+            if i["id"] == j["id"]:
+                found = 1
+        if found:
+            continue
+        else:
+            res.append(i)
+    res.reverse()
+    for i in res:
+            print(f"{i['author']['username']}: {i['content']} ({i['reply_count']} replies)")
+    old = x
